568a4bfeaf7be9a6071105929af389c0906e87a7
UserGPC <UserGPC@DESKTOP-4VVRAQ5>
2024-09-26 16:10:13 -0400

    Agregar archivos de proyecto.

---
+++ CalcularPromedioMatricula/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcularPromedioMatricula
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno alumno = new Alumno
+            {
+
+
+                Nombre = "Juan Pimentel",
+                Matricula = "12345",
+                Carrera = "desarrollo de software",
+                Calificaciones = new List<int> { 85, 90, 78, 92 }
+
+               
+            };
+            alumno.MostrarInformacion();
+        }
+
+     }
+}
---
+++ CalcularPromedioMatricula/alumno.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcularPromedioMatricula
+{
+     class Alumno
+    {
+        public string Nombre { get; set; }
+        public string Matricula { get; set; }
+        public string Carrera { get; set; }
+        public List<int> Calificaciones { get; set; }
+
+        public double CalcularPromedio()
+        {
+            try
+            {
+                int suma = 0;
+                foreach (int calificacion in Calificaciones)
+                {
+                    suma += calificacion;
+                }
+                return (double)suma / Calificaciones.Count;
+
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("ocurrio un error al calcular el promedio:");
+                return 0;
+
+            }
+        }
+
+        public void MostrarInformacion()
+        {
+            Console.WriteLine($"Nombre: {Nombre}");
+            Console.WriteLine($"Matricula: {Matricula}");
+            Console.WriteLine($"Carrera: {Carrera}");
+            Console.WriteLine($"Promedio:{CalcularPromedio()} ");
+        }
+
+
+
+     }
+}
